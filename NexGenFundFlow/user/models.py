7fd1feb9116f2a295f044c80981dc2544b0281b9
abdullah <abdullah.alanezi.23@gmail.com>
2023-12-22 21:54:06 +0300

    add tow model for startup manager and iverstor

--- NexGenFundFlow/user/models.py
+++ NexGenFundFlow/user/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class InverstorProfile(models.Model):
+    invested_campanies =models.TextChoices('invested_campany',['1','2','3','4','5','6','7','8','9','10'])
+    user = models.OneToOneField(User,on_delete = models.CASCADE)
+    inverstor_phone_number = models.CharField(max_length=10)
+    specialization = models.CharField(max_length=255)
+    inverstor_avatar = models.ImageField(upload_to='images/',default='iamges/default.jpg')
+    inverstor_birth_day = models.DateField()
+    inverstor_x_link = models.URLField()
+    inverstor_bio = models.TextField()
+    inverstor_city = models.CharField(max_length=255)
+    inverstor_LinkedIn = models.URLField()
+    invested_campany =  models.CharField(max_length=56,choices=invested_campanies,default='0')
+
+
+
+class StrtatupMangaerProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    position = models.CharField(max_length=255)
+    manager_phone_number = models.CharField(max_length=10)
+    manager_avatar = models.ImageField(upload_to='images/',default='iamges/default.jpg')
+    manager_birth_day = models.DateField()
+    manager_x_link = models.URLField()
+    manager_bio = models.TextField()
+    manager_city = models.CharField(max_length=255)
+    manager_LinkedIn = models.URLField()
+
