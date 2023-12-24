8707b0a6f3b6af5e84087970a15f4aaf7bebc5a4
oudy yousef <oudy.usef@gmail.com>
2023-12-24 13:28:30 +0300

    model InvestmentOffer

--- NexGenFundFlow/fund/models.py
+++ NexGenFundFlow/fund/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+#from startup.models import FundingRound
 
 # Create your models here.
+class InvestmentOffer(models.Model):
+    status = models.TextChoices("status",["Pending","Approved","Disapproved","Canceled"])
+    #funding_round = models.ForeignKey(FundingRound,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    percentage = models.FloatField()
+    amount = models.IntegerField()
+    status = models.CharField(max_length=64,choices=status.choices, default=status.Pending)
+    
+    
