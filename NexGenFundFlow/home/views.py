78077a5c3d2cac80f75ddbee072c22af9808a390
AlenaziHazal <roomyham@MacBook-Pro.local>
2023-12-24 15:04:16 +0300

    home page is ready

--- NexGenFundFlow/home/views.py
+++ NexGenFundFlow/home/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpRequest,HttpResponse
 
 # Create your views here.
+
+def home_view(request:HttpRequest):
+
+    return render(request,'home/home.html')
