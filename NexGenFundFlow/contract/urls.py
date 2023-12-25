c3d3bfe441545c75cab1d19ed725e3a35788a5aa
naif <iimnaif@gmail.com>
2023-12-25 12:01:19 +0300

    add conract

--- NexGenFundFlow/contract/urls.py
+++ NexGenFundFlow/contract/urls.py
@@ -5,5 +5,7 @@ app_name = 'contract'
 
 
 urlpatterns = [
-    
+        path('deal_form/', views.deal_form, name='deal_form'),
+        path('create-deal-pdf/', views.create_deal_pdf, name='create_deal_pdf'),
+
 ]
