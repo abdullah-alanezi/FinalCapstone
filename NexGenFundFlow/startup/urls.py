fc98206f8430422b1819875e02ce21fb10ac86b5
AlenaziHazal <roomyham@MacBook-Pro.local>
2023-12-26 15:03:46 +0300

    finished stratup pforile edit and brows, add team memeber and brows team member

--- NexGenFundFlow/startup/urls.py
+++ NexGenFundFlow/startup/urls.py
@@ -6,4 +6,8 @@ app_name = 'startup'
 
 urlpatterns = [
     path('create/',views.create_startup_view,name='create_startup_view'),
+    path('brows/<startup_id>/',views.view_startup_profile_view,name='view_startup_profile_view'),
+    path('edit/<startup_id>/',views.edit_startup_view,name='edit_startup_view'),
+    path('<startup_id>/team/',views.team_view,name='team_view'),
+    path('<startup_id>/add/team/',views.add_team_member_view,name='add_team_member_view')
 ]
