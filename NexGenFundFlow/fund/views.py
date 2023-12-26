29b03654a515944b3676033aebf1c0254204eb97
oudy yousef <oudy.usef@gmail.com>
2023-12-26 14:58:32 +0300

    investment_requests_view and delete it

--- NexGenFundFlow/fund/views.py
+++ NexGenFundFlow/fund/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render ,redirect
 from django.http import HttpRequest , HttpResponse
-from startup.models import FundingRound
+from startup.models import FundingRound 
+from .models import InvestmentOffer
 
 # Create your views here.
 
@@ -12,7 +13,26 @@ def all_funding_round_view(request:HttpRequest):
 
 def funding_round_details_view(request:HttpRequest,funding_round_id):
     funding_round = FundingRound.objects.get(id = funding_round_id)
+    if request.method == "POST":
+        new_investment_offer = InvestmentOffer(user=request.user ,funding_round=funding_round, percentage=request.POST["percentage"], amount=request.POST["amount"])
+        new_investment_offer.save()
+        
+        return redirect("fund:investment_requests_view")
     
     return render(request,"fund/funding_round_details.html",{"funding_round":funding_round})
+
+def investment_requests_view(request:HttpRequest):
+    investment_requests = InvestmentOffer.objects.filter(user=request.user)
+    count = 0
+    
+    return render(request,"fund/investment_requests.html",{"investment_requests":investment_requests, "count":count})
+
+def cancel_investment_offer_view(request:HttpRequest,investment_offer_id):
+    investment_offer = InvestmentOffer.objects.get(id=investment_offer_id)
+    investment_offer.delete()
+    return redirect("fund:investment_requests_view")
+    
+
+
 
     
